98ec7c5c2a12df10d3ba1b2642d866e8e9d7f431
Darryl Chin <darryl@karmalaunch.com>
2015-09-07 22:20:34 -0700

    comment controllers

--- files/controllers/404.js
+++ files/controllers/404.js
@@ -1,6 +1,8 @@
 module.exports = function(skelly, req, res) {
+  // the view file in /views
   var viewFile = "404.html";
 
+  // data that you want to pass into the view
   var data = {
     path : req.requrl.path
   };
--- files/controllers/index.js
+++ files/controllers/index.js
@@ -1,6 +1,8 @@
 module.exports = function(skelly, req, res) {
+  // the view file in /views
   var viewFile = "index.html";
 
+  // data that you want to pass into the view
   var data = {
     
   };
