fd5da5a2b6ae0a481915ff0a0ba18e347f62936d
Darryl Chin <me@darrylchin.com>
2016-05-20 19:12:21 -0700

    Fix listener to use callback.  Remove var env, which was never used.

--- files/app.js
+++ files/app.js
@@ -1,9 +1,6 @@
 var http = require('http'); // http server
 var skelly = require('skellyjs'); // skellyjs framework
 
-// set the environment from shell variable.  this changes things like api keys, logging, and other configurations (default to development)
-var env = process.env.NODE_ENV || 'development';
-
 // initialize skelly
 skelly.init();
 
@@ -13,6 +10,7 @@ var server = http.createServer(function(req, res) {
 });
 
 // accept incoming traffic
-server.listen(process.env.PORT || 4000);
-skelly.log.debug('Listening on port:', server.address().port);
-skelly.log.debug("Hash:",skelly.hash);
+server.listen({port: process.env.PORT || 5000}, function() {
+	skelly.log.debug('Listening on port:', server.address().port);
+	skelly.log.debug("Hash:",skelly.hash);
+});
